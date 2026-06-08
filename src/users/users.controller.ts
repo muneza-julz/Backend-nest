2c06a12e37f9f009ddd08e6858d848acb44071c3
Muneza Jules Eusèbe <munezajulz@gmail.com>
2026-06-08 22:19:11 +0200

    Request handling → standard approach

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get() // No path needed for query params
+  findByQuery(@Query('id') id?: string) {
+    if (!id) {
+      return [];
+    }
+    return { id };
+  }
+
+  @Get('interns')
+  findInterns() {
+    return {
+      id: 1,
+      name: "Scott McCall",
+      age: 14
+    };
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    if(id != 1)
+        return { 
+        id: id,
+        name: "Prince",
+        age:12
+     }; // better to return an object
+  }       
+
+}
