9aec68cd2b6618715fbb4a5638cab24d2f3ff5eb
Muneza Jules Eusèbe <munezajulz@gmail.com>
2026-06-09 21:00:52 +0200

    injecting dependencies, services inside @Controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,32 +1,27 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor( private readonly usersService: UsersService){}
+
   @Get() // No path needed for query params
-  findByQuery(@Query('id') id?: string) {
-    if (!id) {
-      return [];
+  findByQuery(@Query('id') id?: number) {
+    if(id){
+      return this.usersService.findByQuery(1);
     }
-    return { id };
+    return this.usersService.findAll("ENGINEER");
+    
   }
 
   @Get('interns')
   findInterns() {
-    return {
-      id: 1,
-      name: "Scott McCall",
-      age: 14
-    };
+    return this.usersService.findInterns();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: number) {
-    if(id != 1)
-        return { 
-        id: id,
-        name: "Prince",
-        age:12
-     }; // better to return an object
-  }       
+  findOne(@Param('id') id: number){
+    return this.usersService.findOne(4);
+  }  
 
 }
