94f8fd7698bb9e638c184c51aa862aafc83ba4ff
Muneza Jules Eusèbe <munezajulz@gmail.com>
2026-08-20 23:22:13 +0200

    added the Users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,32 +2,32 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
-     private users = [
-        { "id": 1, "name": "Leanne Graham", "email": "leanne@gmail.com", "role": "INTERN"},
-        { "id": 2, "name": "Ervin Howell", "email": "ervin@gmail.com", "role": "INTERN"},
-        { "id": 3, "name": "Clementine Bauch", "email": "bauch@gmail.com", "role": "ENGINEER"},
-        { "id": 4, "name": "Patricia Lebsack", "email": "lebsack@gmail.com", "role": "ENGINEER"},
-        { "id": 5, "name": "Chelsey Dietrich", "email": "dietrich@gmail.com", "role": "ADMIN"}
-     ]
+   private users = [
+      { "id": 1, "name": "Leanne Graham", "email": "leanne@gmail.com", "role": "INTERN" },
+      { "id": 2, "name": "Ervin Howell", "email": "ervin@gmail.com", "role": "INTERN" },
+      { "id": 3, "name": "Clementine Bauch", "email": "bauch@gmail.com", "role": "ENGINEER" },
+      { "id": 4, "name": "Patricia Lebsack", "email": "lebsack@gmail.com", "role": "ENGINEER" },
+      { "id": 5, "name": "Chelsey Dietrich", "email": "dietrich@gmail.com", "role": "ADMIN" }
+   ]
 
-     findOne( id: number){
-        const user = this.users.find(user => user.id === id);
-        return user;
-     }
-     findAll( role?: 'INTERN' | 'ENGINEER' | 'ADMIN'){
-        if(role){
-            const users = this.users.filter(user => user.role === role);
-            return users;
-        }
-        return this.users;
-     }
-     findByQuery( id: number){
-        const user = this.users.find(user => user.id === id);
-        return user;
-     }
-     findInterns(){
-        const interns = this.users.filter(user => user.role === "INTERN");
-        return interns;
-     }
+   findOne(id: number) {
+      const user = this.users.find(user => user.id === id);
+      return user;
+   }
+   findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+      if (role) {
+         const users = this.users.filter(user => user.role === role);
+         return users;
+      }
+      return this.users;
+   }
+   findByQuery(id: number) {
+      const user = this.users.find(user => user.id === id);
+      return user;
+   }
+   findInterns() {
+      const interns = this.users.filter(user => user.role === "INTERN");
+      return interns;
+   }
 
 }
